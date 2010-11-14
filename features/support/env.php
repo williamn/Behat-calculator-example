c0cdc3cee8f58a40eebe5db3fee26c675265c0dc
William Notowidagdo <wnotowidagdo@gmail.com>
2010-11-14 20:12:20 +0700

    Renaming step file. Remove unnecessary code.

--- features/support/env.php
+++ features/support/env.php
@@ -3,8 +3,5 @@
 require_once 'PHPUnit/Autoload.php';
 require_once 'PHPUnit/Framework/Assert/Functions.php';
 
-$path = '/path/to/your/classes';
-set_include_path(get_include_path() . PATH_SEPARATOR . $path);
-
 require_once 'Calculator.php';
 
